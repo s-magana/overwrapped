d3aad75fda8462337cdad1f59cf14c97c9eed0f9
Steve Magana <steveamagana@gmail.com>
2023-08-14 00:02:26 -0700

    Fix visibility of hash in url and set token into local storage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [token, setToken] = useState('');
+
+    const getParamsFromHash = (hash) => {
+        const hashContent = hash.substr(1); // removes #
+        const paramsSplit = hashContent.split('&'); // returns list with keys and values
+        let params = {}; // fill with params
+        let values = []; // use in foreach loop to store split return values
+        paramsSplit.forEach((item) => {
+            values = item.split('=');
+            params[values[0]] = values[1];
+        });
+        return params;
+    };
+
+    useEffect(() => {
+        setToken(localStorage.getItem('token'));
+    }, [token]);
+
+    useEffect( () => { 
+        // getting the token from the hash
+        if(window.location.hash) {
+            const hash = window.location.hash;
+            const tokens = getParamsFromHash(hash);
+            localStorage.setItem('token', tokens.access_token);
+            setToken(tokens.access_token);
+            window.history.pushState({}, null, '/home');
+        }
+    }, []);
+
     return (
         <div>
-            hey from home!
+            {
+                token && 
+                <div>
+                    <h1>homepage</h1>
+                    <p>here is your token: {token}</p>
+                </div>
+            }
         </div>
     );
 };
